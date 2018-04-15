2521a1402dc587072e76e46c148a32891a50c7c5
Max <mtsk3000@gmail.com>
2018-04-15 21:07:50 +0100

    worked on chrome

--- src/App.js
+++ src/App.js
@@ -1,6 +1,27 @@
 import React, { Component } from 'react';
+import { BrowserRouter as Router } from "react-router-dom";
 
-import Sidenav from './MainNav/index.js';
+
+// Component import
+import SideNav from './Components/Nav/SideNav.js';
+import OldSideNav from './Components/Nav/OldSideNav.js';
+
+import {
+	mainNav1, 
+	settingSideNav1
+} from './Components/Nav/navItems.js';
+
+
+
+// Module import
+
+
+
+// Main app
+
+
+
+
 
 class App extends Component {
   
@@ -8,9 +29,50 @@ class App extends Component {
 
   render() {
     return (
-      <div className="App">
-        <Sidenav></Sidenav>
-      </div>
+    	<Router>
+			<div className="App">
+				{/*<SideNav data={mainNav1} />*/}
+				<OldSideNav />
+				<SideNav 
+					data={settingSideNav1}
+					backgroundColor="white" 
+					textColor="black" 
+					fontSize={12} 
+					additionalClassNames="settingSideNav"
+					width={200}
+					paddingLeft={270}
+				/>
+
+				<div className="module-wrapper">
+				    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				    Why do we use it?
+				    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+				    Where does it come from?
+				    ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum 
+				    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				    Why do we use it?
+				    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+				    Where does it come from?
+				    ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
+				    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				    Why do we use it?
+				    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+				    Where does it come from?
+				    ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
+				    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				    Why do we use it?
+				    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+				    Where does it come from?
+				    ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
+				    Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+				    Why do we use it?
+				    It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
+				    Where does it come from?
+				    ontrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum
+				</div>
+
+			</div>
+		</Router>
     );
   }
 }
---
+++ src/Components/Nav/navItems.js
@@ -0,0 +1,71 @@
+export const mainNav1 = [
+		{
+			title: "Dashboard",
+			path: "/dashboard"
+		},
+		{
+			title: "Checkout",
+			path: "/checkout"
+		},
+		{
+			title: "Affiliates",
+			path: "/affiliates"
+		},
+		{
+			title: "Analytics",
+			path: "/analytics"
+		},
+		{
+			title: "Analytics",
+			path: "/analytics"
+		},
+		{
+			title: "Audience",
+			path: "/audience"
+		},
+		{
+			title: "Team",
+			path: "/team"
+		}
+	]
+
+export const settingSideNav1 = [
+		{
+			title: "Account settings",
+			path: "/vendor-settings/account"
+		},
+		{
+			title: "VAT/Taxes",
+			path: "/vendor-settings/tax"
+		},
+		{
+			title: "Checkout settings",
+			path: "/vendor-settings/checkout-settings"
+		},
+		{
+			title: "Currency Preferences",
+			path: "/vendor-settings/currency-preferences"
+		},
+		{
+			title: "Integrations",
+			path: "/vendor-settings/integrations"
+		},
+		{
+			title: "Alerts",
+			path: "/vendor-settings/alerts"
+		},
+		{
+			title: "Subscriptions",
+			path: "/vendor-settings/subscriptions"
+		},		{
+			title: "Public key",
+			path: "/vendor-settings/public-key"
+		},		{
+			title: "SDK API",
+			path: "/vendor-settings/sdk-api"
+		},		{
+			title: "DevMate integration",
+			path: "/vendor-settings/devmate-integration"
+		},
+	]
+
